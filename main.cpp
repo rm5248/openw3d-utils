4613408efdbc869803432db457e36c27fb677f70
Robert Middleton <robert.middleton@rm5248.com>
2025-03-16 09:22:22 -0700

    working on vcpkg stuff in windows

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include "rmmixviewer.h"
 
 #include <QApplication>
-#include <iostream>
+#include <log4cxx/basicconfigurator.h>
+
+static auto logger = log4cxx::Logger::getLogger("MyApp");
 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    log4cxx::BasicConfigurator::configure();
+
+    LOG4CXX_ERROR(logger, "Starting up");
+
     RMMixViewer w;
     w.setWindowState(Qt::WindowMaximized);
     w.show();
